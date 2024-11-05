325a7ae591a1ba01786c2d05599163efbda80a2d
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-05 12:32:27 +0330

    دادن تنظیمات برای کلی سایت مثل تعریف عنوان سایت ویا متاتگ و یا اضافه کردن ویژگی به تگ بادی

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   // ssr: false,
+  app:{
+    head:{
+      title: 'my Title',
+      meta:[
+        {name: 'description', content: 'My description' },
+      ],
+      bodyAttrs:{
+        class: 'test'
+      }
+    }
+  }
 })
