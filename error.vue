621f00cc34c630cf16d75e87b751c9f122aebd54
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-05 12:26:38 +0330

    صفحه مدیریت خطاها

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const error = useError();
+console.log(error);
+if(error.value?.statusCode === 404) {
+  error.value.message = 'Page Not Found1';
+}
+function handleError(error) {
+    clearError({redirect:'/'})
+}
+
+</script>
+
+<template>
+<div>
+  <h1>Error Page</h1>
+  <button @click="handleError">Home Page</button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
