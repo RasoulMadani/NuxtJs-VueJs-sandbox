e83445c38241bedd45406fc6c55c8b0ce7bc9b93
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-09 13:01:43 +0330

    مدیریت استیت در ناکس جی اس

--- pages/about.vue
+++ pages/about.vue
@@ -1,28 +1,16 @@
 <script setup>
-
-
-useHead(
-    {
-      title: `About`,
-      meta:[
-        {name:'description',content:`About usage`},
-      ],
-      bodyAttrs:{
-        class:'allah'
-      }
-    }
-);
-definePageMeta({
-  layout: 'custom'
-})
+  const count = useState('count',()=>'0');
 </script>
 
 <template>
 
    <div>about</div>
     <NuxtLink to="/">About Page</NuxtLink>
-
-
+    <br>
+    <br>
+  <h1>Count : {{count}}</h1>
+  <button @click="count++">Increment</button>
+  <button @click="count--">Decrement</button>
 </template>
 
 <style scoped>
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+const count = useState('count');
+console.log(count);
 </script>
 
 <template>
   <h1>posts page</h1>
+ <h1>{{count}}fsdf</h1>
 <NuxtPage/>
 </template>
 
