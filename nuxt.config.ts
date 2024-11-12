76dea5da667c94a9a12131e5315b38d180af886d
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-12 13:44:06 +0330

    رفع ایراد جزیی

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules:['@pinia/nuxt', '@nuxt/image'],
-  css:['~/assets/css/main.css','~/assets/scss/main.scss'],
+  modules:['@pinia/nuxt'],
+  css:['~/assets/css/main.css'],
   // ssr: false,
   app:{
     head:{
