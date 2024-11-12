c4cd33dc33be079985e65a9e477415583a940df4
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-12 09:05:57 +0330

    نصب تست به صورت پلاگین و پیکربندی آن به صورت پلاگین

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <script setup>
 
+import {useToast} from "vue-toastification";
+
 const {$hello} = useNuxtApp();
+const toast = useToast();
+function runToast(){
+  toast.success("my toast successfully");
+}
 </script>
 
 <template>
@@ -10,6 +16,8 @@ const {$hello} = useNuxtApp();
   <h2>{{$hello('ali sheikh')}}</h2>
   <NuxtLink to="/about">About Page</NuxtLink>
   <NuxtLink to="/posts">Posts Page</NuxtLink>
+  <br>
+  <button @click="runToast">run toast</button>
 
 </div>
 
